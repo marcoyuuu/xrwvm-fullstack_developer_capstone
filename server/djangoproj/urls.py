041e12e2bfbe191b02c765412406ec1909b9bd32
Marco Yu Cordero <63518626+marcoyuuu@users.noreply.github.com>
2024-11-01 18:00:05 -0400

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -21,8 +21,13 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('djangoapp/', include('djangoapp.urls')),
-    path('', TemplateView.as_view(template_name="Home.html")),
-    path('about/', TemplateView.as_view(template_name="About.html")),
-    path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('djangoapp/', include('djangoapp.urls')),  # Include URLs from djangoapp
+
+    # Paths for static pages
+    path('', TemplateView.as_view(template_name="Home.html"), name='home'),
+    path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
+    path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
+    
+    # Path for React-based login page
+    path('login/', TemplateView.as_view(template_name="index.html"), name='login_page'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
