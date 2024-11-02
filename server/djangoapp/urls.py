664ff70c7ca5b629fef24dc0f712c13038c831e8
Marco Yu Cordero <63518626+marcoyuuu@users.noreply.github.com>
2024-11-02 15:34:30 -0400

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # Path for registration (uncomment if you have a registration view)
-    # path('register/', view=views.register_user, name='register'),
+    # Path for registration
+    path('register/', view=views.registration, name='register'),
 
     # Path for login with trailing slash
     path('login/', view=views.login_user, name='login'),
@@ -14,9 +14,9 @@ urlpatterns = [
     # Path for logout with trailing slash
     path('logout/', view=views.logout_user, name='logout'),
 
-    # Path for dealer reviews view (uncomment if applicable)
+    # Path for dealer reviews view (uncomment and implement if applicable)
     # path('dealer/<int:dealer_id>/', view=views.dealer_details, name='dealer_details'),
 
-    # Path for add a review view (uncomment if applicable)
+    # Path for add a review view (uncomment and implement if applicable)
     # path('dealer/<int:dealer_id>/review/', view=views.add_review, name='add_review'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
