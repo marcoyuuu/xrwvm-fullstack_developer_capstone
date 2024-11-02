13db19825678f76c56393d5794742e4b2e05d74b
Marco Yu Cordero <63518626+marcoyuuu@users.noreply.github.com>
2024-11-02 15:33:31 -0400

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,18 +1,3 @@
-"""djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -28,6 +13,7 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html"), name='about'),
     path('contact/', TemplateView.as_view(template_name="Contact.html"), name='contact'),
     
-    # Path for React-based login page
+    # Path for React-based login and registration pages
     path('login/', TemplateView.as_view(template_name="index.html"), name='login_page'),
+    path('register/', TemplateView.as_view(template_name="index.html"), name='register_page'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
